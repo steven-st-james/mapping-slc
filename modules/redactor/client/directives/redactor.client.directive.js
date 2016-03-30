f83af36ab4c81a3711076134f2990a533dad64a9
Steve <stevensaintjames@yahoo.com>
2016-03-29 22:57:17 -0600

    attempt at redactor

--- modules/redactor/client/directives/redactor.client.directive.js
+++ modules/redactor/client/directives/redactor.client.directive.js
@@ -13,58 +13,72 @@
     .constant('redactorOptions', redactorOptions)
     .directive('redactor', ['$timeout', function ($timeout) {
 
-      var project = {
-        _id: '56f4e0deb11c53c0117d967c'
-      };
+      //var project = {
+      //  _id: '56f4e0deb11c53c0117d967c'
+      //};
 
       return {
         restrict: 'A',
         require: 'ngModel',
         link: function (scope, element, attrs, ngModel) {
-        angular.element('#redactor').redactor({
-          imageUpload:'/api/v1/projects/' + project._id + '/s3/upload'
+                  element.redactor({
+                      imageUpload: '/api/v1/projects/' + project._id + '/s3/upload',
+                      focus: true,
+                      plugins: ['video','imagemanager'],
+                      callbacks: {
+                          autosaveError:function(json){
+                              console.log('Redactor Error: ', json);
+                          }
+                      }
         });
-          // Expose scope var with loaded state of Redactor
-          scope.redactorLoaded = false;
-
-          var updateModel = function updateModel(value) {
-              // $timeout to avoid $digest collision
-              $timeout(function () {
-                scope.$apply(function () {
-                  ngModel.$setViewValue(value);
-                });
-              });
-            },
-            options = {
-              callbacks: {
-                change: updateModel
-              }
-            },
-            additionalOptions = attrs.redactor ?
-              scope.$eval(attrs.redactor) : {},
-            editor;
-
-
-          angular.extend(options, redactorOptions, additionalOptions);
-          console.log(':::options 1::::\n', options);
-          options.imageUpload = '/api/v1/projects/' + project._id + '/s3/upload';
-          console.log(':::options 2::::\n', options);
-          // put in timeout to avoid $digest collision.  call render()
-          // to set the initial value.
-          $timeout(function () {
-            editor = element.redactor(options);
-            console.log(':::editor::::\n', editor);
-            ngModel.$render();
-          });
-
-          ngModel.$render = function () {
-            if (angular.isDefined(editor)) {
-              $timeout(function () {
-                element.redactor('code.set', ngModel.$viewValue || '');
-                scope.redactorLoaded = true;
-              });
+            function saveContent() {
+                ngModel.$render = function(value){
+                    ngModel.$setViewValue(value);
+                    console.log('ngModel: ', ngModel);
+                };
             }
-          };
+
+          // Expose scope var with loaded state of Redactor
+          //scope.redactorLoaded = false;
+          //
+          //var updateModel = function updateModel(value) {
+          //    // $timeout to avoid $digest collision
+          //    $timeout(function () {
+          //      scope.$apply(function () {
+          //        ngModel.$setViewValue(value);
+          //      });
+          //    });
+          //  },
+          //  options = {
+          //    callbacks: {
+          //      change: updateModel
+          //    }
+          //  },
+          //  additionalOptions = attrs.redactor ?
+          //    scope.$eval(attrs.redactor) : {},
+          //  editor;
+          //
+          //
+          //angular.extend(options, redactorOptions, additionalOptions);
+          //console.log(':::options 1::::\n', options);
+          //options.imageUpload = '/api/v1/projects/' + project._id + '/s3/upload';
+          //console.log(':::options 2::::\n', options);
+          //// put in timeout to avoid $digest collision.  call render()
+          //// to set the initial value.
+          //$timeout(function () {
+          //  editor = element.redactor(options);
+          //  console.log(':::editor::::\n', editor);
+          //  ngModel.$render();
+          //});
+          //
+          //ngModel.$render = function () {
+          //  if (angular.isDefined(editor)) {
+          //    $timeout(function () {
+          //      element.redactor('code.set', ngModel.$viewValue || '');
+          //      scope.redactorLoaded = true;
+          //    });
+          //  }
+          //};
 
 
         }
